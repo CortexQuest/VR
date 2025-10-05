f33abb48d9299a56f1795a48f62bff06bedb13e7
DavidMDias <wxv196@gmail.com>
2025-10-05 16:16:27 -0400

    template

---
+++ js/initialise.js
@@ -0,0 +1,19 @@
+/**
+  * @Author David Dias
+  * Initialisation
+  *
+  * @param 	Aucun
+  * @return	true
+  */
+(function()
+{
+	btnsVideo();
+	btnsAfficher();
+  document.querySelector("header").style.opacity = 0;
+  setTimeout(fondu, 4500);
+	changerLangue();
+ return true;
+}) ();
+
+
+
